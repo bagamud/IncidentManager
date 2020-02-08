d4f4c4d89a6f3169a2a8c9cf4d76f91ea8b162b4
bagamud <ponkratovk@gmail.com>
2020-02-08 22:41:13 +0300

    com

---
+++ src/incident/Incident.java
@@ -0,0 +1,10 @@
+package incident;
+
+import java.util.Date;
+
+public class Incident {
+    private int id;
+    private Date date;
+    private String category;
+    private Priority priority;
+}
---
+++ src/incident/Priority.java
@@ -0,0 +1,10 @@
+package incident;
+
+public class Priority {
+    private int priorityCode;
+    private int influence;
+    private int urgency;
+    private String description;
+    private String deadline;
+
+}
