bceb0ed2528613192dacd8cf62bea8c1fa54d360
Kirill Ponkratov <ponkratovk@gmail.com>
2020-02-10 18:20:57 +0300

    com

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,24 @@ public class Main {
         Statement st;
         ResultSet rs;
 
-        Incident incident = new Incident("category", new Priority(0,1), "1 отдел УГИБДД",
-                "Филиппов Дмитрий", "182", "Не печатает принтер", "Гаврилова Н.С.",
+        Incident incident = new Incident(
+                "category",
+                new Priority(0,1),
+                "1 отдел УГИБДД",
+                "Филиппов Дмитрий",
+                "182",
+                "Не печатает принтер",
+                "Гаврилова Н.С.",
                 "в работе");
 
         sql = "INSERT INTO INCIDENT (INCIDENT_DATE, INCIDENT_CATEGORY, INCIDENT_PRIORITY, REQUESTER_DEPARTMENT, " +
                 "REQUESTER, REQUESTER_CONTACTS, IP_ADDRESS, INCIDENT_DURATION, INCIDENT_DESCRIPTION, EXECUTOR, " +
-                "OPERATOR, INCIDENT_STATUS) VALUES (" + incident.getIncidentDate() + ", '" + incident.getIncidentCategory() +
-                "', '" + incident.getIncidentPriority().getDescription() + "', '" + incident.getRequesterDepartment() + "', '" + incident.getRequester() +
-                "', '" + incident.getRequesterContacts() + "', '" + incident.getIpAddress() + "', " + incident.getIncidentDuration() +
-                ", '" + incident.getIncidentDescription() + "', " + incident.getExecutor() + "', '" + incident.getOperator() + "', " +
+                "OPERATOR, INCIDENT_STATUS) VALUES ('" + incident.getIncidentDate() + "', '"
+                + incident.getIncidentCategory() + "', '" + incident.getIncidentPriority().getDescription() + "', '"
+                + incident.getRequesterDepartment() + "', '" + incident.getRequester() + "', '"
+                + incident.getRequesterContacts() + "', '" + incident.getIpAddress() + "', "
+                + incident.getIncidentDuration() + ", '" + incident.getIncidentDescription() + "', '"
+                + incident.getExecutor() + "', '" + incident.getOperator() + "', '" +
                 incident.getIncidentStatus() + "')";
 
         System.out.println(sql);
