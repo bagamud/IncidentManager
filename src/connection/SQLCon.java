d959b8387b0eaa3aadf3914c08e3749275062bd9
Kirill Ponkratov <ponkratovk@gmail.com>
2020-02-10 14:08:14 +0300

    com

---
+++ src/connection/SQLCon.java
@@ -0,0 +1,36 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLCon {
+    private String url = "jdbc:mysql://localhost:3306/incidentManager";
+    private String username = "user";
+    private String password = "password";
+    private Connection connection;
+
+    public Connection connect() {
+        if (connection == null) {
+            System.out.println("Connecting database...");
+            try {
+                connection = DriverManager.getConnection(url, username, password);
+                System.out.println("Connected!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+    public Connection disconnect() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+                System.out.println("Connection database closed.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
