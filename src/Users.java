43ed590ab7161126ba5de6d2fcea73881237d533
bagamud <ponkratovk@gmail.com>
2020-03-28 14:53:59 +0300

    Com

--- src/Users.java
+++ src/Users.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.KeyStore;
 
 public class Users {
     private String userLogin;
@@ -11,6 +10,7 @@ public class Users {
     private String userEmail;
 
     public Users() throws IOException {
+        //TODO: Сделать нормальное шифрование и хэширование пароля
         BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
         System.out.print("Enter login: ");
         userLogin = reader.readLine();
@@ -22,6 +22,5 @@ public class Users {
         userGroup = reader.readLine();
         System.out.print("Enter e-mail: ");
         userEmail = reader.readLine();
-
     }
 }
