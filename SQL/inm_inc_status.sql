b92d74c0745708fa1a8377547dad71e7055276c5
Kirill Ponkratov <ponkratovk@gmail.com>
2020-08-12 20:45:55 +0300

    INM-1 Отредактированы пользовательские данные и созданы файлы вставки данных

---
+++ SQL/inm_inc_status.sql
@@ -0,0 +1,3 @@
+INSERT INTO inm.inc_status (ID, TITLE) VALUES (1, 'В работе');
+INSERT INTO inm.inc_status (ID, TITLE) VALUES (2, 'Отложен');
+INSERT INTO inm.inc_status (ID, TITLE) VALUES (3, 'Решен');
